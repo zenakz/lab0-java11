0db6ce38a25fec9b1f1f969928090c4cd4b75004
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-02-28 11:48:08 +0100

    lab_0 TailsGenerator class added

---
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -0,0 +1,13 @@
+package edu.iis.mto.lab_0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TailsGenerator {
+
+	public List<String> tails(String value) {
+		List<String> tails = new ArrayList<>(value.length());		
+		return tails;
+	}
+
+}
