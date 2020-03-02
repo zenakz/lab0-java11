340ceaf7a03c8652785c4150d4a08f099e6eede9
Zientak.David <215934@edu.p.lodz.pl>
2020-03-02 18:05:32 +0100

    changed formatting

--- src/test/java/edu/iis/mto/lab_0/TailsGeneratorTest.java
+++ src/test/java/edu/iis/mto/lab_0/TailsGeneratorTest.java
@@ -14,27 +14,23 @@ class TailsGeneratorTest {
     private TailsGenerator tailsGenerator;
     private List<String> tails;
 
-    @BeforeEach
-    public void setup() {
+    @BeforeEach public void setup() {
 
         tailsGenerator = new TailsGenerator();
         tails = tailsGenerator.tails(HELLO);
     }
 
-    @Test
-    public void tailsShouldReturnListOfStringsOfSizeEqualsToInputStringLengthPlusOne() {
+    @Test public void tailsShouldReturnListOfStringsOfSizeEqualsToInputStringLengthPlusOne() {
 
         assertThat(tails, Matchers.hasSize(HELLO.length() + 1));
     }
 
-    @Test
-    public void tailsShouldReturnFullStringAsFirstElement() {
+    @Test public void tailsShouldReturnFullStringAsFirstElement() {
 
         assertThat(tails.get(0), Matchers.equalTo(HELLO));
     }
 
-    @Test
-    public void tailsShouldReturnEmptyStringAsLastElement() {
+    @Test public void tailsShouldReturnEmptyStringAsLastElement() {
 
         assertThat(tails.get(tails.size() - 1), Matchers.equalTo(""));
     }
